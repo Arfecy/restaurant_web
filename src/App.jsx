3eae4d40c1e162349e51ec1431dd31c71af4932f
Arfecy <alessandro0223@gmail.com>
2024-08-07 16:28:19 +0200

    added section

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,13 @@ function App() {
         <a href='#'>Menu</a>
         <a href='#'>About</a>
       </div>
+      <section className='section'>
+        <h2>Welcome to Tapa Tapa!</h2>
+        <p>At Tapa Tapa, we bring you a delightful culinary experience inspired by the rich traditions of Croatian food. Nestled in the heart of Zagreb, our restaurant offers a cozy ambiance, impeccable service, and a menu crafted with the freshest ingredients.
+
+          Whether you're joining us for a casual lunch, a romantic dinner, or a special celebration, our team is dedicated to making your visit memorable. Explore our diverse menu, savor our chef's specialties, and enjoy an unforgettable dining experience.
+          Come dine with us and discover the perfect blend of flavors, creativity, and hospitality. We look forward to serving you!</p>
+      </section>
     </>
   )
 }
